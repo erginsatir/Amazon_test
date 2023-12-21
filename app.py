50edecddb83a251cee099e5a681f548636590f48
erginsatir <80199331+erginsatir@users.noreply.github.com>
2023-12-21 23:26:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ def pull_amazon_data(url):
             product_data_list.append({
                 "name": sel.css("#productTitle::text").get("").strip(),
                 "price": sel.css('.a-price-whole::text').get() + '.' + sel.css('.a-price-fraction::text').get(),
+                "price_3": sel.css('.a-offscreen::text').get() ,
+                "price_4": sel.css('.a-price::text').get() ,
                 "price_2":  sel.xpath(price_xpath).get(),
                 "stars": sel.css("i[data-hook=average-star-rating] ::text").get("").strip(),
                 "rating_count": sel.css("#acrCustomerReviewText::text").get("").strip(),
