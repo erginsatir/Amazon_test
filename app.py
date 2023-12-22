7b549e15a49da92b3ddd38bd023d4ae477a81648
erginsatir <80199331+erginsatir@users.noreply.github.com>
2023-12-22 13:25:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,16 +18,9 @@ def pull_amazon_data(url):
             price_xpath = '//td[@class="a-color-secondary" and contains(text(), "Price:")]/following-sibling::td/span[contains(@class, "a-price")]/span[@class="a-offscreen"]/text()'
 
             product_data_list.append({
-                "name": sel.css("#productTitle::text").get("").strip(),
-                "price": sel.css('.a-price-whole::text').get() + '.' + sel.css('.a-price-fraction::text').get(),
-                "price_3": sel.css('.a-offscreen::text').get() ,
-                "price_4": sel.css('.a-price-fraction::text').get() ,
-                "price_5": sel.css('.a-tex-price::text').get() ,
-                "price_2":  sel.xpath(price_xpath).get(),
-                "stars": sel.css("i[data-hook=average-star-rating] ::text").get("").strip(),
-                "rating_count": sel.css("#acrCustomerReviewText::text").get("").strip(),
-                "feature_bullets": feature_bullets,
-                "variant_data": variant_data,
+
+                "price_3": sel.css('.product-list__price::text').get() ,
+
             })
     except Exception as e:
             print("Error", e)
